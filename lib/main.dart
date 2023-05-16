651aced9815dae9ab89dfffde1a4804de5d90a17
goosari0123 <goosari0123@gmail.com>
2023-05-17 00:38:29 +0900

    0517 인터페이스 추가
    1. 로그인 인터페이스 추가
    2. 회원가입 인터페이스 추가
    3. 태그 추가 인터페이스 수정
    
    4. 태그 포멧 양식 수정(서버와 연동되게)
    
    On branch develop_0517
    Changes to be committed:
    	modified:   lib/main.dart
    	modified:   lib/screens/map.dart
    
    Changes not staged for commit:
    	modified:   Teemo_frontend (modified content)
    
    Untracked files:
    	.vscode/
    	lib/func/tagmake_func.dart
    	lib/screens/login.dart
    	lib/screens/signup.dart
    	lib/screens/tagmake.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:teemo_front/screens/map.dart';
+import 'package:teemo_front/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MapScreen(),
+      //home: MapScreen(),
+      home: LoginPage(),
     );
   }
 }
